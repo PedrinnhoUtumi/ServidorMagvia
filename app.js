876f4e9d0b4994ffa66b659a95094db7c6b98422
PedrinnhoUtumi <brunopena454@gmail.com>
2025-04-28 14:54:49 -0300

    .

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
-const express = require('express')
-const cors = require('cors')
-const axios = require('axios')
+const express = require('express');
+const cors = require('cors');
+const axios = require('axios');
 
 const app = express();
-const port = 1883; 
+const port = process.env.PORT || 1883;  // <--- agora porta dinâmica!
 
-app.use(cors());  
-app.use(express.json()); 
+app.use(cors());
+app.use(express.json());
 
 const url = 'http://192.168.10.250:5654/db/query';
 
@@ -18,40 +18,39 @@ app.get('/', (req, res) => {
             <meta charset="UTF-8">
             <meta name="viewport" content="width=device-width, initial-scale=1.0">
             <meta name="theme-color" content="#FFAA00" />
-
             <title>Informações do ISBN</title>
         </head>
         <body>
-            <h1 style='background-color: #FFAA00;'>Ola Mundo</h1>
+            <h1 style='background-color: #FFAA00;'>Olá Mundo</h1>
         </body>
         </html>
-        `)
-})
+    `);
+});
 
 app.get('/api/machbase', (req, res) => {
-  const query = {
-    q: `SELECT * FROM ARCOND`,
-    format: "json",
-    timeformat: "default",
-    tz: "local",
-    precision: 2,
-  };
+    const query = {
+        q: `SELECT * FROM ARCOND`,
+        format: "json",
+        timeformat: "default",
+        tz: "local",
+        precision: 2,
+    };
 
-  axios.post(url, query, {
-    auth: {
-      username: 'sys',
-      password: 'manager',
-    }
-  })
+    axios.post(url, query, {
+        auth: {
+            username: 'sys',
+            password: 'manager',
+        }
+    })
     .then((response) => {
-      res.json(response.data); 
+        res.json(response.data);
     })
     .catch((error) => {
-      console.error('Erro ao conectar com o Machbase:', error);
-      res.status(500).json({ error: 'Erro ao conectar com o Machbase' });
+        console.error('Erro ao conectar com o Machbase:', error.message);
+        res.status(500).json({ error: 'Erro ao conectar com o Machbase' });
     });
 });
 
 app.listen(port, () => {
-  console.log(`Servidor Node.js rodando na porta ${port}`);
+    console.log(`Servidor Node.js rodando na porta ${port}`);
 });
