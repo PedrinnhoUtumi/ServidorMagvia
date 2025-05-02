e5e7618ce48ab14d9348f5121e850d7b1609ed57
PedrinnhoUtumi <146356179+wellytomOC@users.noreply.github.com>
2025-05-02 15:51:06 -0300

    Update server.js

--- server.js
+++ server.js
@@ -28,7 +28,7 @@ app.get('/', (req, res) => {
 });
 
 app.get('/api', async (req, res) => {
-  const tabelas = ['ACTIVEPOWER', 'VOLTAGE', 'CURRENT', 'BUSINESS', 'CONSUMPTION', 'GENERATED'];
+  const tabelas = ['ACTIVEPOWER', 'VOLTAGE', 'CURRENT', 'BUSINESS', 'CONSUMPTION', 'GENERATED', 'MYUSER'];
 
   try {
       const resultados = await Promise.all(tabelas.map(tabela => {
