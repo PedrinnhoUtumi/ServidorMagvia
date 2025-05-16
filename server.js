260dabe2b63e0fe5925fb452fa722592bbe1e778
PedrinnhoUtumi <130059527+PedrinnhoUtumi@users.noreply.github.com>
2025-05-16 15:55:40 -0300

    Update server.js

--- server.js
+++ server.js
@@ -71,7 +71,7 @@ app.get("/api", async (req, res) => {
   }
 });
 
-app.get("/api/MYUSER", async (req, res) => {
+app.post("/api/MYUSER", async (req, res) => {
   const { nome, email, senha, role, account } = req.body;
 
   try {
