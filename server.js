e095c032e139f5ca085160dbc23f1798f8caf804
PedrinnhoUtumi <130059527+PedrinnhoUtumi@users.noreply.github.com>
2025-05-27 17:05:28 -0300

    Update server.js

--- server.js
+++ server.js
@@ -99,7 +99,7 @@ app.get("/api", async (req, res) => {
     const resultados = await Promise.all(
       tabelas.map((tabela) => {
         const query = {
-          q: `SELECT * FROM ${tabela} ORDER BY ID`,
+          q: `SELECT * FROM ${tabela}`,
           format: "json",
           timeformat: "default",
           tz: "local",
