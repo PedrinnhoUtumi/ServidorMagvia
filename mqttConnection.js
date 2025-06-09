c9cea071aa90cec9d40497170c2487a4fcfd76ed
PedrinnhoUtumi <130059527+PedrinnhoUtumi@users.noreply.github.com>
2025-06-09 15:22:31 -0300

    .

---
+++ mqttConnection.js
@@ -0,0 +1,43 @@
+const allInsertsController = require("./controller/allInserts.controller");
+const mqtt = require("mqtt");
+const mqttUrl = "ws://192.168.10.250:5654/web/api/mqtt";
+
+const mqttOptions = {
+  clean: true,
+  connectTimeout: 4000,
+  protocolVersion: 5,
+};
+
+const mqttClient = mqtt.connect(mqttUrl, mqttOptions);
+exports.conexaoMQTT = () => {
+    let ultimoDadoMQTT = {}; 
+    mqttClient.on("connect", () => {
+      console.log("Conectado ao broker MQTT");
+      mqttClient.subscribe("/api/energia/monitor", (err) => {
+        if (!err) {
+          console.log("Inscrito no tópico /api/energia/monitor");
+        } else {
+          console.error("Erro ao se inscrever:", err);
+        }
+      });
+    });
+    
+    mqttClient.on("message", (topic, message) => {
+      console.log("Tópico:", topic);
+      console.log("Mensagem recebida:", message.toString());
+      if (topic === "/api/energia/monitor") {
+        const dados = JSON.parse(message.toString());
+        console.log("Dados recebidos:", dados);
+        ultimoDadoMQTT = {
+          timestamp: new Date().toISOString(),
+          ...dados,
+        };
+        allInsertsController.adicionaTodosOsDados(ultimoDadoMQTT)
+      }
+    });
+    
+    mqttClient.on("error", (err) => {
+      console.error("Erro no MQTT:", err.message);
+    });
+    return ultimoDadoMQTT;
+}
--- server.js
+++ server.js
@@ -1,22 +1,15 @@
 const express = require("express");
 const cors = require("cors");
 require("dotenv").config(); 
-const axios = require("axios");
 const bodyParser = require("body-parser");
 const app = express();
 const PORT = process.env.PORT || 3000;
-const mqtt = require("mqtt");
 
-const allInsertsController = require("./controller/allInserts.controller");
 const todasAsTabelasController = require("./controller/todasAsTabelas.controller");
 const myUserController = require("./controller/myUser.controller");
 const tabelasSemInsercaoController = require("./controller/tabelasSemInsercao.controller");
+const mqttConnection = require("./mqttConnection");
 
-const activePower = require("./entities/activePower");
-const voltage = require("./entities/voltage");
-const current = require("./entities/current");
-const consumption = require("./entities/consumption");
-const generation = require("./entities/generation");
 const myUser = require("./entities/myUser");
 
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -25,65 +18,10 @@ app.use(bodyParser.json());
 app.use(cors());
 app.use(express.json());
 
-let ultimoDadoMQTT = {}; 
-
-const url = "http://192.168.10.250:5654/db/query";
-const mqttUrl = "ws://192.168.10.250:5654/web/api/mqtt";
-
-const mqttOptions = {
-  clean: true,
-  connectTimeout: 4000,
-  protocolVersion: 5,
-};
-
-const mqttClient = mqtt.connect(mqttUrl, mqttOptions);
-
-mqttClient.on("connect", () => {
-  console.log("Conectado ao broker MQTT");
-  mqttClient.subscribe("/api/energia/monitor", (err) => {
-    if (!err) {
-      console.log("Inscrito no tópico /api/energia/monitor");
-    } else {
-      console.error("Erro ao se inscrever:", err);
-    }
-  });
-});
-
-mqttClient.on("message", (topic, message) => {
-  console.log("Tópico:", topic);
-  console.log("Mensagem recebida:", message.toString());
-  if (topic === "/api/energia/monitor") {
-    const dados = JSON.parse(message.toString());
-    console.log("Dados recebidos:", dados);
-    ultimoDadoMQTT = {
-      timestamp: new Date().toISOString(),
-      ...dados,
-    };
-    allInsertsController.adicionaTodosOsDados(ultimoDadoMQTT)
-  }
-});
-
-mqttClient.on("error", (err) => {
-  console.error("Erro no MQTT:", err.message);
-});
-
-
+mqttConnection.conexaoMQTT()
 
 app.get("/", (req, res) => {
-  res.send(`
-        <!DOCTYPE html>
-        <html lang="pt-BR">
-        <head>
-            <meta charset="UTF-8">
-            <meta name="viewport" content="width=device-width, initial-scale=1.0">
-            <meta name="theme-color" content="#FFAA00" />
-            <title>API Machbase</title>
-        </head>
-        <body>
-            <h1 style='background-color: #FFAA00;'>Olá Mundo</h1>
-        </body>
-        </html>
-    `);
+  res.send(`Servidor ONLINE! Acesse a API em http://localhost:${PORT}/api`);
 });
 
 app.get("/api", async (req, res) => {
@@ -103,8 +41,6 @@ app.get("/api", async (req, res) => {
   }
 });
 
-
-
 app.post("/api/MYUSER", async (req, res) => {
   const { nome, email, senha, role, account } = req.body;
   const novoUsuario = new myUser(nome, email, senha, role, account);
@@ -120,7 +56,3 @@ app.post("/api/MYUSER", async (req, res) => {
 app.listen(PORT, '0.0.0.0', () => {
   console.log(`Servidor Node.js rodando na porta ${PORT}`);
 });
-
-// SALVANDO COMANDOS
-// ngrok config add-authtoken 2wN8FoCzGqhT2tAoannVHp31mFY_p7ZPVxQF9H5EfiwLBSVh
-// ngrok http 192.168.10.250:5654
