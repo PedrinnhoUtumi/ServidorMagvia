a86a4f6943544eae8ce0b0c09a4e21c8ea76193f
“Bellin1” <130068559+brunpena@users.noreply.github.com>
2025-07-24 08:32:35 -0300

    .

---
+++ dist/assets/RotaProtegida-5pjZlan_.js
@@ -0,0 +1,1 @@
+import{j as a,f as e,O as o}from"./index-Dhav9A0X.js";function u(){const t=localStorage.getItem("usuario");return t!==null&&t!==""?a.jsx(o,{}):a.jsx(e,{to:"/",replace:!0})}export{u as default};
